53f20cf209605428c19ed9c910f340294d3fdd15
dreamer603 <mj617066@gmail.com>
2024-07-16 19:53:21 +0900

    Singleton Add and JsonData Completed

---
+++ Assets/Scripts/JsonData.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BuildingInfo
+{
+    private int[] _pos;
+    private string _building;
+    
+    public BuildingInfo() { }
+
+    public BuildingInfo(int[] pos, string building)
+    {
+        _pos = pos;
+        _building = building;
+    }
+
+    public int[] Pos
+    {
+        get { return _pos; }
+        set { _pos = value; }
+    }
+
+    public string Building
+    {
+        get { return _building; }
+        set { _building = value; }
+    }
+}
+
+public class JsonData
+{
+    private string _userName;
+    private int _userLevel;
+
+    private float _dollars;
+    private List<BuildingInfo> _buildingInfos;
+    private string[] _workers;
+    
+    private float _oxygen;
+    private float _water;
+    private float _electricity;
+
+    private int _rices;
+    private int _corns;
+    private int _potatoes;
+
+    public JsonData() { }
+
+    public JsonData(string userName, int userLevel, float dollars,  List<BuildingInfo> buildingInfos, string[] workers, float oxygen,
+        float water, float electricity, int rices, int corns, int potatoes)
+    {
+        _userName = userName;
+        _userLevel = userLevel;
+        _dollars = dollars;
+        _buildingInfos = buildingInfos;
+        _workers = workers;
+        _oxygen = oxygen;
+        _water = water;
+        _electricity = electricity;
+        _rices = rices;
+        _corns = corns;
+        _potatoes = potatoes;
+    }
+
+    public string UserName
+    {
+        get { return _userName; }
+        set { _userName = value; }
+    }
+
+    public int UserLevel
+    {
+        get { return _userLevel; }
+        set { _userLevel = value; }
+    }
+
+    public float Dollars
+    {
+        get { return _dollars; }
+        set { _dollars = (float)Math.Floor(value * 100) / 100; }
+    }
+
+    public List<BuildingInfo> BuildingInfos
+    {
+        get { return _buildingInfos; }
+        set { _buildingInfos = value; }
+    }
+
+    public string[] Workers
+    {
+        get { return _workers; }
+        set { _workers = value; }
+    }
+
+    public float Oxygen
+    {
+        get { return _oxygen; }
+        set { _oxygen = (float)Math.Floor(value * 100) / 100; }
+    }
+
+    public float Water
+    {
+        get { return _water; }
+        set { _water = (float)Math.Floor(value * 100) / 100; }
+    }
+
+    public float Electricity
+    {
+        get { return _electricity; }
+        set { _electricity = (float)Math.Floor(value * 100) / 100; }
+    }
+
+    public int Rices
+    {
+        get { return _rices; }
+        set { _rices = value; }
+    }
+
+    public int Corns
+    {
+        get { return _corns; }
+        set { _corns = value; }
+    }
+
+    public int Potatoes
+    {
+        get { return _potatoes; }
+        set { _potatoes = value; }
+    }
+    
+}
--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,17 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
-public class Singleton : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static T _instance;
+
+    public static T Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+    
+                if (_instance == null)
+                {
+                    GameObject gameObject = new GameObject(typeof(T).Name, typeof(T));
+                    _instance = gameObject.GetComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        
+        if (transform.parent != null && transform.root != null)
+        {
+            DontDestroyOnLoad(this.transform.root.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        Init();
+    }
+
+    protected virtual void Init()
     {
         
     }
