d2bedc4672bac5332ce4d67044306b761c932383
hongdu08 <kimdh04150416@gmail.com>
2024-07-16 22:04:15 +0900

    Sounds:: add
    소리추가함

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,6 +5,39 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioClip homeBGM;
+    public AudioClip marketBGM;
+    [Header("get")]
+    public AudioClip getWater;
+    public AudioClip getGrain;
+    public AudioClip getVoltage;
+    public AudioClip getAir;
+    public AudioClip getEthanol;
+    [Header("click")]
+    public AudioClip clickWaterStorage;
+    public AudioClip clickGrainStorage;
+    public AudioClip clickVoltageStorage;
+    public AudioClip clickAirStorage;
+    public AudioClip clickEthanolStorage;
+    public AudioClip clickMarket;
+    public AudioClip clickBase1;
+    public AudioClip clickBase2;
+    public AudioClip clickWaterProduction;
+    public AudioClip clickGrainProduction;
+    public AudioClip clickVoltageProduction;
+    public AudioClip clickAirProduction;
+    public AudioClip clickEthanolProduction;
+    [Header("gita")]
+    public AudioClip buy;
+    public AudioClip sell;
+    public AudioClip marketIn;
+    public AudioClip marketOut;
+    public AudioClip constructionStart;
+    public AudioClip clickConstruction;
+    public AudioClip constructionEnd;
+
+
+    
     void Start()
     {
         
