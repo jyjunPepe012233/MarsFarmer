eb0e87867b0b882dbde9953315692e69534c7318
dreamer603 <mj617066@gmail.com>
2024-07-17 19:53:03 +0900

    UI Test Add

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
+    private string _userName;
+    private int userLevel;
+    
     protected override void Init()
     {
         
@@ -13,4 +16,45 @@ public class UIManager : Singleton<UIManager>
     {
         Debug.Log("Building Click");
     }
+
+    public void OnClickMapping()
+    {
+        Debug.Log("Mapping Click");
+    }
+
+    public void OnClickProductList()
+    {
+        Debug.Log("Product List Click");
+    }
+
+    public void OnClickWorkerList()
+    {
+        Debug.Log("Worker List Click");    
+    }
+
+    public void OnClickMarket()
+    {
+        Debug.Log("Market Click");
+    }
+
+    public void OnClickWorkerGotcha()
+    {
+        Debug.Log("Worker Gotcha Click");
+    }
+
+    public void OnClickPost()
+    {
+        Debug.Log("Post Click");
+    }
+
+    public void OnClickQuest()
+    {
+        Debug.Log("Quest Click");
+    }
+
+    public void OnClickSetting()
+    {
+        Debug.Log("Setting Click");
+    }
+    
 }
